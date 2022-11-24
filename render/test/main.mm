742d812e7a7945b788b6c45690e93bdb6d741f78
mizt <info@mizt.org>
2022-11-24 14:48:55 +0900

    Update

--- render/test/main.mm
+++ render/test/main.mm
@@ -7,8 +7,8 @@
 #import "../lunasvg/include/lunasvg.h"
 
 void Quad(NSMutableString *svg, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-    [svg appendString:[NSString stringWithFormat:@"<polylines points=\"%d,%d %d,%d %d,%d %d,%d %d,%d\" />",
-        x1,y1,x2,y2,x3,y3,x4,y4,x1,y1]];
+    [svg appendString:[NSString stringWithFormat:@"<polygon points=\"%d,%d %d,%d %d,%d %d,%d\" />",
+        x1,y1,x2,y2,x3,y3,x4,y4]];
 
 }
 
